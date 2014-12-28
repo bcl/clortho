5e190dfbde959604e6341d10a76f3df942c33edb
Brian C. Lane <bcl@redhat.com>
2014-12-28 11:29:03 -0800

    Add README and COPYING
    Add GPLv2 license and a simple README explaning how things work.

--- src/clortho.py
+++ src/clortho.py
@@ -1,3 +1,23 @@
+# clortho - A simple key/value server
+#
+# Copyright 2014 by Brian C. Lane <bcl@brianlane.com>
+# All Rights Reserved
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Author(s): Brian C. Lane <bcl@brianlane.com>
+#
 import os
 import asyncio
 import signal
